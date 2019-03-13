a6eb737d9f7e4c557c219976cb02fbd96db0ba12
Halil <kilinchalil96@gmail.com>
2019-03-13 14:38:15 +0300

    Upgrade.

--- app/src/main/java/com/example/gameapp/app/activity/MainActivity.java
+++ app/src/main/java/com/example/gameapp/app/activity/MainActivity.java
@@ -1,79 +1,30 @@
 package com.example.gameapp.app.activity;
 
-import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
-
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.gameapp.app.R;
-import com.example.gameapp.app.adapter.CustomAdapter;
-import com.example.gameapp.app.model.RetroPhoto;
-import com.example.gameapp.app.network.GetDataService;
-import com.example.gameapp.app.network.RetrofitClientInstance;
-
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CustomAdapter adapter;
-    private RecyclerView recyclerView;
-    ProgressDialog progressDoalog;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressDoalog = new ProgressDialog(MainActivity.this);
-        progressDoalog.setMessage("Loading...");
-        progressDoalog.show();
+        Button sign = findViewById(R.id.signButton);
 
-        /*Create handle for the RetrofitInstance interface*/
-        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
-
-        Call<List<RetroPhoto>> call = service.getAllPhotos();
-
-        call.enqueue(new Callback<List<RetroPhoto>>() {
-
-
+        sign.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
-                progressDoalog.dismiss();
-                generateDataList(response.body());
-            }
-
-            @Override
-            public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
-                progressDoalog.dismiss();
-                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,GameRecyclerActivity.class);
+                startActivity(intent);
             }
         });
-
-
-
     }
-
-
-
-    /*Method to generate List of data using RecyclerView with custom adapter*/
-    private void generateDataList(List<RetroPhoto> photoList) {
-        recyclerView = findViewById(R.id.customRecyclerView);
-        adapter = new CustomAdapter(this,photoList);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
-
-    }
-
-
-
-
 }
+
